cc8a5cd5e6163ffbf89267bceb827a7710461201
sandoval <juansandoval.cv@gmail.com>
2026-08-19 09:37:16 -0600

    creacion de la rama estadistica

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,124 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnCalcular = new Button();
-            lblDe = new Label();
-            lblA = new Label();
-            txtCantidad = new Label();
-            tbCantidad = new TextBox();
-            lblRespuesta = new Label();
-            cboDe = new ComboBox();
-            cboA = new ComboBox();
             SuspendLayout();
-            // 
-            // btnCalcular
-            // 
-            btnCalcular.Location = new Point(284, 316);
-            btnCalcular.Name = "btnCalcular";
-            btnCalcular.Size = new Size(177, 73);
-            btnCalcular.TabIndex = 0;
-            btnCalcular.Text = "calcular";
-            btnCalcular.UseVisualStyleBackColor = true;
-            btnCalcular.Click += button1_Click;
-            // 
-            // lblDe
-            // 
-            lblDe.AutoSize = true;
-            lblDe.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblDe.Location = new Point(96, 69);
-            lblDe.Name = "lblDe";
-            lblDe.Size = new Size(32, 20);
-            lblDe.TabIndex = 1;
-            lblDe.Text = "de:";
-            lblDe.Click += label1_Click;
-            // 
-            // lblA
-            // 
-            lblA.AutoSize = true;
-            lblA.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblA.Location = new Point(396, 69);
-            lblA.Name = "lblA";
-            lblA.Size = new Size(23, 20);
-            lblA.TabIndex = 3;
-            lblA.Text = "a:";
-            // 
-            // txtCantidad
-            // 
-            txtCantidad.AutoSize = true;
-            txtCantidad.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtCantidad.Location = new Point(96, 195);
-            txtCantidad.Name = "txtCantidad";
-            txtCantidad.Size = new Size(81, 20);
-            txtCantidad.TabIndex = 5;
-            txtCantidad.Text = "cantidad:";
-            // 
-            // tbCantidad
-            // 
-            tbCantidad.Location = new Point(183, 192);
-            tbCantidad.Name = "tbCantidad";
-            tbCantidad.Size = new Size(125, 27);
-            tbCantidad.TabIndex = 6;
-            tbCantidad.TextChanged += textBox3_TextChanged;
-            // 
-            // lblRespuesta
-            // 
-            lblRespuesta.AutoSize = true;
-            lblRespuesta.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblRespuesta.Location = new Point(396, 195);
-            lblRespuesta.Name = "lblRespuesta";
-            lblRespuesta.Size = new Size(109, 20);
-            lblRespuesta.TabIndex = 7;
-            lblRespuesta.Text = "respuesta: ??";
-            // 
-            // cboDe
-            // 
-            cboDe.DropDownStyle = ComboBoxStyle.DropDownList;
-            cboDe.FormattingEnabled = true;
-            cboDe.Items.AddRange(new object[] { "Metros", "Cm ", "Pulgadas ", "Pies", "Varas", "Yardas", "Km", "Millas " });
-            cboDe.Location = new Point(133, 66);
-            cboDe.Name = "cboDe";
-            cboDe.Size = new Size(151, 28);
-            cboDe.TabIndex = 8;
-            // 
-            // cboA
-            // 
-            cboA.DropDownStyle = ComboBoxStyle.DropDownList;
-            cboA.FormattingEnabled = true;
-            cboA.Items.AddRange(new object[] { "Metros", "Cm ", "Pulgadas ", "Pies", "Varas", "Yardas", "Km", "Millas " });
-            cboA.Location = new Point(425, 66);
-            cboA.Name = "cboA";
-            cboA.Size = new Size(151, 28);
-            cboA.TabIndex = 9;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(cboA);
-            Controls.Add(cboDe);
-            Controls.Add(lblRespuesta);
-            Controls.Add(tbCantidad);
-            Controls.Add(txtCantidad);
-            Controls.Add(lblA);
-            Controls.Add(lblDe);
-            Controls.Add(btnCalcular);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
+ 
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Button btnCalcular;
-        private Label lblDe;
-        private Label lblA;
-        private Label txtCantidad;
-        private TextBox tbCantidad;
-        private Label lblRespuesta;
-        private ComboBox cboDe;
-        private ComboBox cboA;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,39 +7,8 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-        }
-        /*
-         metros, cm, pulgadas, pies, varas, yardas, km, millas
-        */
-        double[] valores = { 1, 100, 39.3701, 3.28084, 1.1963, 1.09361, 0.001, 0.000621371 };
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int de = cboDe.SelectedIndex, a = cboA.SelectedIndex;
-                double cantidad = double.Parse(tbCantidad.Text);
-
-                double respuesta = valores[a] / valores[de] * cantidad;
-                lblRespuesta.Text = respuesta.ToString("N2");
-            }
-            catch 
-            {
-                lblRespuesta.Text = "La operación falló, utiliza caracteres numéricos válidos";
-            }
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
+        
+        
 
         }
     }
